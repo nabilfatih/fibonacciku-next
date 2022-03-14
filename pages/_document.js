f06046f5c5e4e4e997da26f8a1a59f43fe4bd225
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-14 18:12:18 +0100

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="id">
+        <Head>
+          <meta charset="UTF-8"></meta>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
