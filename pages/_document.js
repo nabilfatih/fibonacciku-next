34385383f7444cbf57cda928303a8b3b62ed4788
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-14 22:37:48 +0100

    update document

--- pages/_document.js
+++ pages/_document.js
@@ -5,25 +5,21 @@ class MyDocument extends Document {
     return (
       <Html lang="id">
         <Head>
-          <meta charset="UTF-8"></meta>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
-          ></meta>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossorigin
-          ></link>
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
         <body>
-          <Main></Main>
+          <Main/>
           <NextScript />
         </body>
       </Html>
