e69f7abac2101cbf9fc628842b406acbb967a0dc
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-16 12:48:50 +0100

    add pelajaran component

--- components/pelajaran-ujian/pelajaran.js
+++ components/pelajaran-ujian/pelajaran.js
@@ -1,7 +1,28 @@
-import './pelajaran-ujian.module.scss'
+import styles from "./pelajaran-ujian.module.scss";
+import cls from "classnames";
 
 const Pelajaran = () => {
-  return <div>Pelajaran</div>;
+  return (
+    <section className={styles.mata_pelajaran}>
+      <div className={cls(styles.pelajaran, "container container--pall")}>
+        <div className={styles.pelajaran__intro}>
+          <h1>Pilih Pelajaran Sesuka Kamu!</h1>
+        </div>
+
+        <div className={styles.pelajaran__grid}>
+          <div className={styles.pelajaran__card}>
+            <div className={styles.pelajaran__icon}></div>
+
+            <div className={styles.pelajaran__title}>
+              <h3></h3>
+            </div>
+
+            <a className={cls(styles.button, "button")}>Belajar Disini!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Pelajaran;
--- pages/mata-pelajaran/index.js
+++ pages/mata-pelajaran/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Footer from "../../components/footer/footer";
 import NavBar from "../../components/nav/nav";
+import Pelajaran from "../../components/pelajaran-ujian/pelajaran";
+import Ujian from "../../components/pelajaran-ujian/ujian";
 
 export default function MataPelajaran() {
   return (
@@ -10,7 +12,8 @@ export default function MataPelajaran() {
       </Head>
 
       <NavBar />
-
+      <Pelajaran />
+      <Ujian />
       <Footer />
     </div>
   );
