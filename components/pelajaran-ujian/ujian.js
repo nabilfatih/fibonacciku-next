1b795e9c837d40652e44396c55d3907e2a1c8a05
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-16 12:57:20 +0100

    add ujian component

--- components/pelajaran-ujian/ujian.js
+++ components/pelajaran-ujian/ujian.js
@@ -1,7 +1,28 @@
-import "./pelajaran-ujian.module.scss";
+import styles from "./pelajaran-ujian.module.scss";
+import cls from "classnames";
 
 const Ujian = () => {
-  return <div>Ujian</div>;
+  return (
+    <section className={styles.persiapan_ujian}>
+      <div className={cls(styles.ujian, "container container--pall")}>
+        <div className={styles.ujian__intro}>
+          <h1>Belajar Persiapan Ujian!</h1>
+        </div>
+
+        <div className={styles.ujian__grid}>
+          <div className={styles.ujian__card}>
+            <div className={styles.ujian__icon}></div>
+
+            <div className={styles.ujian__title}>
+              <h3></h3>
+            </div>
+
+            <a className={cls(styles.button, "button")}>Belajar Disini!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Ujian;
