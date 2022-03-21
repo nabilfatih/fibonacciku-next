07e1e60c2b82812f2ca106e82c196e02da5e3170
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-21 19:08:05 +0100

    add ssdg

--- pages/mata-pelajaran/[query].js
+++ pages/mata-pelajaran/[query].js
@@ -1,12 +1,27 @@
+import { useRouter } from "next/router";
+
 import Head from "next/head";
 import Footer from "../../components/footer/footer";
 import NavBar from "../../components/nav/nav";
 
+import dataBab from "../../data/dataBab.json";
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+}
+
 const Query = () => {
+  const router = useRouter();
+  // console.log(getBabData);
+
   return (
     <div>
       <Head>
-        <title>| FibonacciKu</title>
+        <title> | FibonacciKu</title>
       </Head>
 
       <NavBar />
