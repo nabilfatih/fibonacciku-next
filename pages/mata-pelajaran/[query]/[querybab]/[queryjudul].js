88a3e4ffc80a16dfbea419c9c5b0886d4351c8ac
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-22 15:27:41 +0100

    add queryjudul

---
+++ pages/mata-pelajaran/[query]/[querybab]/[queryjudul].js
@@ -0,0 +1,12 @@
+import Footer from "../../../../components/footer/footer";
+import NavBar from "../../../../components/nav/nav";
+
+export default function QueryJudul() {
+    return (
+        <div>
+            <NavBar />
+
+            <Footer />
+        </div>
+    )
+}
