efd45d15094f4cfbbc655b91de0364c179b032aa
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-22 15:25:28 +0100

    fixed bugs

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>FibonacciKu | Belajar Gratis Kapanpun Dimanapun</title>
         <meta
           name="description"
