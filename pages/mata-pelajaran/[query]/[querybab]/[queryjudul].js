839c02ad7a956b4e2e341fbd43f2c789196a4981
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-23 09:48:51 +0100

    fixed bugs

--- pages/mata-pelajaran/[query]/[querybab]/[queryjudul].js
+++ pages/mata-pelajaran/[query]/[querybab]/[queryjudul].js
@@ -1,12 +1,45 @@
+import Head from "next/head";
 import Footer from "../../../../components/footer/footer";
 import NavBar from "../../../../components/nav/nav";
 
-export default function QueryJudul() {
-    return (
-        <div>
-            <NavBar />
+import dataKonten from "../../../../data/dataKonten.json";
 
-            <Footer />
-        </div>
-    )
+export async function getStaticPaths() {
+  return {
+    paths: dataKonten.map((konten) => {
+      return {
+        params: {
+          query: konten.query,
+          querybab: konten.querybab,
+          queryjudul: konten.queryjudul,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const konten = dataKonten.find(
+    (konten) => konten.queryjudul === params.queryjudul
+  );
+
+  return {
+    props: {
+      konten,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default function QueryJudul({ konten }) {
+  return (
+    <div>
+      <Head>
+        <title>{konten.judul}</title>
+      </Head>
+      <NavBar />
+      <div>{konten.judul}</div>
+      <Footer />
+    </div>
+  );
 }
