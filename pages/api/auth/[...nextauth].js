8e51c8bba49e9e924ae143b4d93eb176c614fe54
nabilfatih <nabilakbarazzima@gmail.com>
2022-03-24 11:12:43 +0100

    add mongodb

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+  ],
+});
