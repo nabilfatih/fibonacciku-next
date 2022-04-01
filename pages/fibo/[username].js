8bc90ed1d09b51185cd79346d72cd3db178b32b7
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-01 11:00:43 +0200

    add profile page

--- pages/fibo/[username].js
+++ pages/fibo/[username].js
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Footer from "../../components/footer/footer";
 import NavBar from "../../components/nav/nav";
 import { useRouter } from "next/router";
+import styles from "./fibo.module.scss";
+import Image from "next/image";
+import cls from "classnames";
 
 export default function Profile() {
   const router = useRouter();
@@ -15,7 +18,60 @@ export default function Profile() {
 
       <NavBar />
 
-      <main></main>
+      <main>
+        <section className={styles.profils}>
+          <div className={styles.profil}>
+            <div className={styles.profil__background}>
+              <Image
+                className={styles.img}
+                src={"/static/img/default-background.webp"}
+                layout="fill"
+                alt={`Profil ${username} Background`}
+                priority
+              />
+            </div>
+
+            <div className={cls(styles.container, "container container--px")}>
+              <div className={styles.profil__info}>
+                <div className={styles.profil__avatar}>
+                  <Image
+                    className={styles.img}
+                    src={"/static/img/Foto.jpg"}
+                    alt={`Foto Profile ${username} FibonacciKu`}
+                    width={128}
+                    height={128}
+                  />
+                  <div className={styles.edit_foto}>
+                    <form>
+                      <label
+                        className={styles.custom_foto_upload}
+                        htmlFor="avatar"
+                        name="avatar"
+                      >
+                        Ganti Foto
+                      </label>
+                      <input
+                        className={styles.custom_file_input}
+                        type="file"
+                        id="avatar"
+                        name="avatar"
+                        accept="image/png, image/jpg, image/jpeg"
+                      />
+                    </form>
+                  </div>
+                </div>
+
+                <div className={styles.profil__username}>
+                  <div className={styles.user}>
+                    <h2>{username}</h2>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
 
       <Footer />
     </div>
