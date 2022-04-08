9feda7c9dcafdcd618bbc8cd74c73c3d158d1f0f
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-08 08:53:47 +0200

    add 500 pages error

--- pages/500.js
+++ pages/500.js
@@ -1,3 +1,36 @@
+import Head from "next/head";
+import styles from "../styles/500.module.scss";
+import Image from "next/image";
+
 export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>;
+  return (
+    <div>
+      <Head>
+        <title>Server Maintenance | FibonacciKu</title>
+      </Head>
+
+      <main>
+        <section className={styles.maintenance}>
+          <div className={styles.background} />
+
+          <div className={styles.container}>
+            <div className={styles.card}>
+              <div className={styles.icon}>
+                <Image
+                  src={"/static/img/logofibonama.svg"}
+                  alt="Logo FibonacciKu"
+                  width={256}
+                  height={48}
+                  priority
+                />
+              </div>
+
+              <h3>Server Maintenance</h3>
+              <p>Harap kembali beberapa saat lagi 😇</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
