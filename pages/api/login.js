a6b246584d42c27dbd02aa2a9f8b6b70fd2ffe49
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-09 19:21:44 +0200

    fixed

--- pages/api/login.js
+++ pages/api/login.js
@@ -32,7 +32,7 @@ export default async function Login(req, res) {
         res.status(201).json({
           token,
           user: { username, _id, nama, avatar },
-          referer: referer.toString(),
+          referer: referer,
           message: "Welcome to FibonacciKu 🤩",
         });
       } else {
