5cc561b67b67514ed536bd4eaf73829c12f83185
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-09 16:09:44 +0200

    fixed

--- pages/masuk/index.js
+++ pages/masuk/index.js
@@ -40,7 +40,7 @@ export default function Masuk() {
       }
     }
     fetchData();
-  }, []);
+  }, [router, token, user]);
 
   const [usernameActive, setUsernameActive] = useState("");
   const [passwordActive, setPasswordActive] = useState("");
