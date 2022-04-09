f5a31396dec3a63da12365c84ef701f1b52065a5
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-09 18:50:12 +0200

    fixed

--- pages/masuk/index.js
+++ pages/masuk/index.js
@@ -112,8 +112,8 @@ export default function Masuk() {
       const { data } = await axios.post(`/api/login`, formData, config);
       cookie.set("token", data?.token, { expires: 3 });
       cookie.set("user", JSON.stringify(data?.user), { expires: 3 });
-      toast.dismiss(loading);
       await router.push(data.referer);
+      toast.dismiss(loading);
       toast.success(data.message, toastConfig);
     } catch (e) {
       toast.dismiss(loading);
