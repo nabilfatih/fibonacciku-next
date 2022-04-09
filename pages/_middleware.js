2acb39a17bbecec40ede47cd526805671433b9e1
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-09 14:42:03 +0200

    update middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,10 +1,21 @@
 import { NextResponse } from "next/server";
-import { verifyToken } from "../lib/utils";
+import jwt from "jsonwebtoken";
 
 export async function middleware(req) {
   const user = req ? req.cookies?.user : null;
   const token = req ? req.cookies?.token : null;
-  const userId = await verifyToken(token);
+  let userId;
+
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      userId = decodedToken?.userId;
+    } catch (e) {
+      userId = null;
+    }
+  } else {
+    userId = null;
+  }
 
   const { pathname } = req.nextUrl;
 
