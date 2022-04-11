f01a3dd2f97873a2d00c9899d98353afe20f69df
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-11 10:49:39 +0200

    update firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -13,7 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
 export { storage };
