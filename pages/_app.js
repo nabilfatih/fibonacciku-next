765fb7a46a55eabde72f09edb6be490ef1002758
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-27 10:06:53 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,6 @@ function MyApp({ Component, pageProps: { ...pageProps } }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-
       <Progress isAnimating={isAnimating} />
       <ToastContainer
         position="top-center"
