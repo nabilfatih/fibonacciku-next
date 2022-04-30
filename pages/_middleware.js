e6716f66083bab3853ace4bab2e76cdadbb15e0a
nabilfatih <nabilakbarazzima@gmail.com>
2022-04-30 11:27:46 +0200

    update

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,7 +4,6 @@ import { verifyToken } from "../lib/utils";
 export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
   const userId = await verifyToken(token);
-
   const { pathname } = req.nextUrl;
 
   if (
